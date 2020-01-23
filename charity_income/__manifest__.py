923ceb20aec0e30f41349edc739b58dde637cef6
root <root@imperza-srv8038.server.net>
2020-01-23 12:00:24 +0800

    charitydec

---
+++ charity_income/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Charity Income',
+    'version': '13.0.0.0.1',
+    'category': 'Income',
+    'summary': 'Mymasjid module to record charity fund receive',
+    'sequence': '2',
+    'author': 'Aamir',
+    'license': 'LGPL-3',
+    'company': 'TCM',
+    'maintainer': 'TCM',
+    'support': 'amir.hi575@gmail.com',
+    'website': '',
+    'depends': [],
+    'live_test_url': '',
+    'demo': [],
+    'data': [
+        'charity.xml'
+
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'images': ['images/accounts.jpeg'],
+    'qweb': [],
+}
---
+++ charity_income/charity.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+
+from odoo import models, fields
+
+class CharityDetail(models.Model):
+    _name = 'charity.detail'
+    _description = 'Mymasjid Charity Record'
+
+
