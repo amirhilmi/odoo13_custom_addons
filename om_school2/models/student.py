534eb37a34ee8d84fd5c91d4e1fabff82f36fbad
root <root@imperza-srv8038.server.net>
2020-01-26 16:22:45 +0800

    om_school_finished

--- om_school2/models/student.py
+++ om_school2/models/student.py
@@ -5,9 +5,21 @@ from odoo import models, fields
 
 class SchoolStudent(models.Model):
     _name = 'school.student2'
-    _inherit = ['mail.thread','mail.activity.mixin']
+    # _inherit = ['mail.thread','mail.activity.mixin']
     _description = "Student Table"
-    # _rec_name = "analytic_id"
-    # _order = "sequence"
 
     name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    guardian = fields.Char(string="Guardian")
+    note = fields.Text(string="Notes")
+    gender = fields.Selection
+    (
+        [
+            ('male', 'Male'),
+            ('female', 'Female'),
+            ('other', 'Other'),
+
+        ], 
+        string='Gender', default='male'
+    )
+    image = fields.Binary(string:"Image")
